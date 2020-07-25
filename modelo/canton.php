68891e4f56edf0fc9fed2c1da966ff270b79d3a0
Gabriel Barboza Carvajal <gabriel.barboza.carvajal.inge@gmail.com>
2020-07-24 19:29:41 -0600

    Update canton.php

--- modelo/canton.php
+++ modelo/canton.php
@@ -1,18 +1,9 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of canton
  *
  * @author Gabriel Barboza Carvajal .
  */
-//include './modelo/division.php';
-//include './modelo/distrito.php';
 class canton extends division {
 
     public $distritos;
